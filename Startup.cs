4153dcb96c25498a02513df8be1c3836187e4808
lizz <lizz@sensetime.com>
2020-02-22 23:25:14 +0800

    list
    Signed-off-by: lizz <lizz@sensetime.com>

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace viewer
@@ -42,6 +44,12 @@ namespace viewer
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine("/mnt/hdd/data/flows/hmdb-rgb")),
+                RequestPath = "/StaticFiles"
+            });
 
             app.UseRouting();
 
